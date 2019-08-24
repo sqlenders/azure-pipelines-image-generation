9d52ea7c62f49a9ada364e50028b625fafdc8e7a
sqlenders <Endsbzng+github@xs4all.nl>
2019-08-24 21:23:14 +0200

    Two extension installation scripts
    Since SSDT is no longer avaible in VS2019 these scripts will install Analysis Services Projects and Report Services Projects. When they are avaible you can build and those projects

---
+++ images/win/scripts/Installers/Vs2019/Extensions/Install-VSIX-AnalysisService.ps1
@@ -0,0 +1,11 @@
+﻿################################################################################
+##  File:  Install-VSIX-AnalysisService.ps1
+##  Team:  ??
+##  Desc:  Installs Microsoft.DataTools.AnalysisServices.vsix in Visual Studio
+################################################################################
+
+Import-Module -Name ImageHelpers -Force
+
+$VSIXName = "Microsoft.DataTools.AnalysisServices.vsix"
+$Url      = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ProBITools/vsextensions/MicrosoftAnalysisServicesModelingProjects/2.8.17/vspackage" 
+$exitcode = Install-VSIX -Name $VSIXName  -Url $Url
---
+++ images/win/scripts/Installers/Vs2019/Extensions/Install-VSIX-ReportingServices.ps1
@@ -0,0 +1,12 @@
+﻿################################################################################
+##  File:  Install-VSIX-ReportingServices.ps1
+##  Team:  ??
+##  Desc:  Installs Microsoft.DataTools.ReportingServices.vsix in Visual Studio
+################################################################################
+
+Import-Module -Name ImageHelpers -Force
+
+$VSIXName = "Microsoft.DataTools.ReportingServices.vsix"
+$Url      = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ProBITools/vsextensions/MicrosoftReportProjectsforVisualStudio/2.5.9/vspackage" 
+$exitcode = Install-VSIX -Name $VSIXName  -Url $Url
+
