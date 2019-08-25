4e44fa9865e1122987bc0fa5d36ad92586a5f498
sqlenders <Endsbzng+github@xs4all.nl>
2019-08-25 14:57:05 +0200

    Two validation files added
    Pretty easy. Just check if file exists

---
+++ images/win/scripts/Installers/Vs2019/Extensions/Validate-VSIX-AnalysisService.ps1
@@ -0,0 +1,18 @@
+﻿################################################################################
+##  File:  Validate-VSIX-AnalysisService.ps1
+##  Team:  ???
+##  Desc:  Validate VSIX AnalysisService Extension in VS2019
+################################################################################
+
+$VSIXName  = "Microsoft Analysis Services Projects Extension"
+$VSIXFilelocation = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\SSAS\Shared\Microsoft.DataTools.Shared.vsix"
+
+if (Test-Path $VSIXFilelocation) 
+{
+    Write-Host "$VSIXName is installed"
+}
+else
+{
+    Write-Error "$VSIXName is NOT installed in '$VSIXFilelocation'"
+    exit 1
+}
---
+++ images/win/scripts/Installers/Vs2019/Extensions/Validate-VSIX-ReportingService.ps1
@@ -0,0 +1,18 @@
+﻿################################################################################
+##  File:  Validate-VSIX-ReportingService.ps1
+##  Team:  ???
+##  Desc:  Validate VSIX ReportingService Extension in VS2019
+################################################################################
+
+$VSIXName  = "Microsoft Reporting Services Projects Extension"
+$VSIXFilelocation = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\CommonExtensions\Microsoft\SSRS\Shared\Microsoft.DataTools.Shared.vsix" 
+
+if (Test-Path $VSIXFilelocation) 
+{
+    Write-Host "$VSIXName is installed"
+}
+else
+{
+    Write-Error "$VSIXName is NOT installed in '$VSIXFilelocation'"
+    exit 1
+}
